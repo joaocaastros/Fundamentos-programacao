da768650fb8995cf30ead33062c129b62ee7a092
joao vicente ferreira de castro <1716896>
2019-10-03 10:37:49 -0300

    exercicios laco

---
+++ laco02.py
@@ -0,0 +1,9 @@
+# Primeiro exemplo de laco
+# ---> Outro laco infinito
+# ---> Nao tente isso em casa
+
+x = 99
+
+while ( x >= 0) :
+    print(x)
+    x = x - 1
---
+++ laco03.py
@@ -0,0 +1,9 @@
+# Um laco para pertguntar o nome da pesssoa
+# e dar uma saudacao
+
+controle = 0
+
+while (controle < 5):
+    nome = input("Qual o seu nome?: ")
+    print("Ola" , nome)
+    controle = controle + 1
